041c50e2ced1330fa458682d7102d086d0775ca6
Rudnev777 <tarmpr77@icloud.com>
2025-12-18 17:47:00 +0300

    phoyo

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
 import * as Utils from "../function/function.js"
 
 Utils.create_breadcrumbs();
-Utils.create_catalog("Автоаксессуары", "../public/auto-goods.png","auto_goods.html" )
-Utils.create_catalog("Запчасти", "../public/parts.png","auto_parts.html" )
+Utils.create_catalog("Автоаксессуары", "public/auto-goods.png","auto_goods.html" )
+Utils.create_catalog("Запчасти", "public/parts.png","auto_parts.html" )
 
 
 fetch('https://api.localhost3000')
