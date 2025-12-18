5c8f48f58993196c6b750632f90f3ab96525ea43
Rudnev777 <tarmpr77@icloud.com>
2025-12-18 17:52:03 +0300

    hfdsjkgdfjghkdf

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import * as Utils from "../function/function.js"
 
 Utils.create_breadcrumbs();
-Utils.create_catalog("Автоаксессуары", "public/auto-goods.png","auto_goods.html" )
+Utils.create_catalog("Автоаксессуары", "test/public/auto-goods.png","auto_goods.html" )
 Utils.create_catalog("Запчасти", "public/parts.png","auto_parts.html" )
 
 
